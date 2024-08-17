04958eeab7b899b07cbf9ef9309a48b122db7185
SeaKarate <141030415+CreeperHunter508@users.noreply.github.com>
2024-08-17 21:25:31 +0530

    Initial commit

---
+++ src/main/java/com/kjthedev/badutility/BadUtility.java
@@ -0,0 +1,15 @@
+package com.kjthedev.badutility;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BadUtility implements ModInitializer {
+	public static final String MOD_ID = "bad-hack";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/kjthedev/badutility/BadUtilityClient.java
@@ -0,0 +1,10 @@
+package com.kjthedev.badutility;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BadUtilityClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
